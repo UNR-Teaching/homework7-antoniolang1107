2ed96832515a69c32548567dd58584d78798bb2d
antoniolang1107 <TonylsLame@users.noreply.github.com>
2021-12-04 11:23:14 -0800

    Starting files

---
+++ dictionary.h
@@ -0,0 +1,15 @@
+#ifndef DICTIONARY
+#define DICTIONARY
+
+class Dictionary : public DictionaryInterface {
+    bool isEmpty() const;
+    int getNumberOfEntries() const;
+    bool add(const KeyType& newKey, const ValueType& newValue);
+    bool remove(const KeyType& targetKey);
+    bool clear();
+    bool contains(const KeyType& targetKey) const;
+    ValueType getValue(const KeyType& targetKey) const;
+};
+
+#include "dictionary.cpp"
+#endif
